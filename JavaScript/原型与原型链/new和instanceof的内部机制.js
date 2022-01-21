95bc1e7eac954abde83dba9cfb711117f0f7b241
anyJohn <33611794+anyJohn@users.noreply.github.com>
2022-01-21 11:51:34 +0800

    WIP: instanceof的内部机制

--- JavaScript/原型与原型链/new和instanceof的内部机制.js
+++ JavaScript/原型与原型链/new和instanceof的内部机制.js
@@ -4,24 +4,25 @@ function Person(name) {
   this.name = name;
 }
 function myNew(func, ...args) {
-  let obj = {};
-  // let proto = Object.getPrototypeOf(obj);
-  // proto = Person.prototype;
-  obj.__proto__ = Person.prototype;
-  /* 上面两行也可以写成
-      Object.create(Person.prototype)
-  */
+  // 创建一个基于构造函数原型的空对象
+  let obj = Object.create(func.prototype);
+  // 等同于
+  /**
+  let obj = {}
+  obj.__proto__ = func.prototype
+   */
   const foo = func.apply(obj, args);
   return foo instanceof Object ? foo : args;
 }
 let p = myNew(Person, "john");
-console.log(p.__proto__);
 
+// 2.instanceof
 function O() {}
 function F() {}
 
 let obj = new O();
 O.prototype = new F();
+console.log(obj.__proto__.prototype.a);
 console.log(obj instanceof O);
 console.log(obj instanceof F);
 console.log(O.prototype instanceof F);
