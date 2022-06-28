fc293fdfd30687aba58cef2d6a7018af8362b85d
anyJohn <33611794+anyJohn@users.noreply.github.com>
2022-06-28 20:36:14 +0800

    chore: bm17

---
+++ 算法/hot100/查找&排序/BM17 二分查找-I.js
@@ -0,0 +1,28 @@
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ *
+ * @param nums int整型一维数组
+ * @param target int整型
+ * @return int整型
+ */
+function search(nums, target) {
+  // write code here
+  if (!nums.length) return -1;
+  let left = 0;
+  let right = nums.length - 1;
+  while (right >= left) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return -1;
+}
+module.exports = {
+  search: search,
+};
