30f2e1ad835aa395bfd858300346a0cf14e462c9
anyJohn <33611794+anyJohn@users.noreply.github.com>
2022-07-02 20:23:41 +0800

    chore: bm33

---
+++ 算法/hot100/二叉树/BM33 二叉树的镜像.js
@@ -0,0 +1,26 @@
+/*
+ * function TreeNode(x) {
+ *   this.val = x;
+ *   this.left = null;
+ *   this.right = null;
+ * }
+ */
+/**
+ * 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+ *
+ * @param pRoot TreeNode类
+ * @return TreeNode类
+ */
+function Mirror(pRoot) {
+  // write code here
+  if (!pRoot) return null;
+  let temp = pRoot.left;
+  pRoot.left = pRoot.right;
+  pRoot.right = temp;
+  Mirror(pRoot.left);
+  Mirror(pRoot.right);
+  return pRoot;
+}
+module.exports = {
+  Mirror: Mirror,
+};
