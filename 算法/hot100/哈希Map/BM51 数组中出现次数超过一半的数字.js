be23fcaeb3f32458a39df1e8cce58a842b16f4cd
anyJohn <33611794+anyJohn@users.noreply.github.com>
2022-07-05 19:07:33 +0800

    refactor: bm51

--- 算法/hot100/哈希Map/BM51 数组中出现次数超过一半的数字.js
+++ 算法/hot100/哈希Map/BM51 数组中出现次数超过一半的数字.js
@@ -1,3 +1,8 @@
+/**
+ * 选举法，
+ * @param {*} numbers
+ * @returns
+ */
 function MoreThanHalfNum_Solution(numbers) {
   // write code here
   if (numbers.length === 1) return numbers[0];
@@ -12,6 +17,29 @@ function MoreThanHalfNum_Solution(numbers) {
     }
   }
   return cur;
+}
+/**
+ * 哈希map法，双O(n)
+ * @param {*} numbers
+ * @returns
+ */
+function MoreThanHalfNum_Solution(numbers) {
+  // write code here
+  if (numbers.length === 1) return numbers[0];
+  let map = {};
+  for (const item of numbers) {
+    if (map[item] === undefined) {
+      map[item] = 1;
+    } else {
+      map[item] += 1;
+    }
+  }
+  for (const item of numbers) {
+    if (map[item] > Math.floor(numbers.length / 2)) {
+      return item;
+    }
+  }
+  return 0;
 }
 module.exports = {
   MoreThanHalfNum_Solution: MoreThanHalfNum_Solution,
